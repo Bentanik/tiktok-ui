1ab5bc1e8adfb13278bdf8d9564d7bc72458ebc3
Bentanik <kennyjamqw@gmail.com>
2023-09-20 21:16:26 +0700

    BuildUI1

--- src/components/Layout/components/Header/index.js
+++ src/components/Layout/components/Header/index.js
@@ -1,12 +1,35 @@
 import classNames from "classnames/bind";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faCircleXmark,
+  faMagnifyingGlass,
+  faSpinner,
+} from "@fortawesome/free-solid-svg-icons";
+
 import styles from "./header.module.scss";
+import images from "~/assets/images";
 
 const cx = classNames.bind(styles);
 
+console.log(images.logo);
 function Header() {
   return (
     <header className={cx("wrapper")}>
-      <div className={cx("inner")}></div>
+      <div className={cx("inner")}>
+        <img src={images.logo} alt="TikTok" />
+
+        <div className={cx("search")}>
+          <input type="text" placeholder="Search accounts and videos" />
+          <button className={cx("clear")}>
+            <FontAwesomeIcon icon={faCircleXmark} />
+          </button>
+          <FontAwesomeIcon className={cx("loading")} icon={faSpinner} />
+          <button className={cx("search-btn")}>
+            <FontAwesomeIcon icon={faMagnifyingGlass} />
+          </button>
+        </div>
+        <div className={cx("action")}>Action</div>
+      </div>
     </header>
   );
 }
